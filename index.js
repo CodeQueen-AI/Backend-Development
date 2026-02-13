8734de0912dc08c771dff55047da5666795a9dec
CodeQueen-AI <codeq209@gmail.com>
2026-02-13 23:34:44 +0500

    update

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const fs = require('node:fs');
 
+// Write
 fs.writeFile("index.txt", 'Hello! How are you? ', function(err){
+    if(err) console.error(err);
+    else console.log("done")
+})
+
+// Append
+fs.appendFile("index.txt", 'I am fine!', function(err){
     if(err) console.error(err);
     else console.log("done")
 })
