ebb4f3720235f6ad700165e31ede7c05d73b308e
CodeQueen-AI <codeq209@gmail.com>
2026-02-13 16:35:38 +0500

    map

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
 // Array
-var arr = [1,2,3,4];
+let arr = [1,2,3,4];
 
 // forEach
-arr.forEach(function(val){
-    console.log(val + " hello")
-})
+// arr.forEach(function(val){
+//     console.log(val + " hello")
+// })
 
+// Map
+var newarr = arr.map(function(val){
+    return 13;
+})
+console.log(newarr)
