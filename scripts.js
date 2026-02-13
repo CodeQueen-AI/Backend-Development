419feb10b5e25dc332d1879e23a71c5f1d89c440
CodeQueen-AI <codeq209@gmail.com>
2026-02-13 19:51:46 +0500

    update

--- scripts.js
+++ scripts.js
@@ -61,6 +61,12 @@
 // var ans = abcd()
 
 // 4-Asynchronous Javascript Coding
-var blob =  await fetch(`https://randomuser.me/api/`);
-var res = await blob.json();
-console.log(res);
+// var blob =  await fetch(`https://randomuser.me/api/`);
+// var res = await blob.json();
+// console.log(res);
+
+async function abcd(){
+    var blob = await fetch(`https://randomuser.me/api/`);
+    var ans = blob.json();
+    console.log(ans)
+}
