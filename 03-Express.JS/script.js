2710f270ba8a863119c8d3f56691990feab259ec
CodeQueen-AI <codeq209@gmail.com>
2026-02-15 23:09:29 +0500

    update

--- 03-Express.JS/script.js
+++ 03-Express.JS/script.js
@@ -2,12 +2,17 @@ import express from 'express';
 const app = express()
 
 // Home Page
-app.get('/' , (req, res) => {
-    res.send('Hello World')
-})
+// app.get('/' , (req, res) => {
+//     res.send('Hello World')
+// })
 
 // Routes - About Routes
-app.get('/about' , (req , res) => {
-    res.send('Hello World from About Page!!')
-})
-app.listen(3000)
+// app.get('/about' , (req , res) => {
+//     res.send('Hello World from About Page!!')
+// })
+// app.listen(3000)
+
+// Middleware
+app.use(function(req , res, next)) {
+    console.log('Run Middleware')
+}
