003a0e7602a3fedc8a9f36000c7b9fb4543e19da
CodeQueen-AI <codeq209@gmail.com>
2026-02-17 16:48:42 +0500

    update

--- 01-Fundamentals of JS/script.js
+++ 01-Fundamentals of JS/script.js
@@ -1,29 +1,3 @@
-
-
-
-
-
-
-
-// Return Value
-function abcd(){
-    return "Hello Wrold!";
-}
-var ans = abcd()
-
-// Arrow Function
-const greet = () => {
-    console.log("Hello");
-};
-
-greet();
- 
-// Function Length
-function add(a, b, c){
-    return a + b + c
-}
-consolelog(add.length)
-
 // 4-Asynchronous Javascript Coding
 async function abcd(){
     var blob = await fetch(`https://randomuser.me/api/`);
