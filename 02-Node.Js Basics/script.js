9620b2b7942e0b460e6bfcbd4377f9e0dea1b9fa
CodeQueen-AI <codeq209@gmail.com>
2026-02-19 19:26:22 +0500

    update

--- 02-Node.Js Basics/script.js
+++ 02-Node.Js Basics/script.js
@@ -31,8 +31,8 @@ const http = require('http');
 const server = http.createServer(function(req, res){
     res.end("Hello World");
 })
-
-
 server.listen(3000);
 
 // 3-Https
+const https = require('https')
+const fs = require('fs')
