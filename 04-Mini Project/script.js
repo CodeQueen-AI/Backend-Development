04481cbd1b7772fadba8d974d9407f920af1d685
CodeQueen-AI <codeq209@gmail.com>
2026-02-22 00:59:12 +0500

    update

--- 04-Mini Project/script.js
+++ 04-Mini Project/script.js
@@ -1,16 +1,1 @@
 
-
-// app.get("/profile/codequeen", (req, res) => {
-//     res.send('CodeQueen Profile');
-// });
-
-// dynamic route
-app.get("/profile/:name", (req, res) => {
-    res.send(`Welcome, ${req.params.name}`);
-});
-
-// Age
-
-
-app.listen(3000)
-
