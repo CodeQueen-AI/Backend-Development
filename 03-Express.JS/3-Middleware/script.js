c2034256256d2225656cfa9408914a5b6654c38e
CodeQueen-AI <codeq209@gmail.com>
2026-02-23 15:27:19 +0500

    update

--- 03-Express.JS/3-Middleware/script.js
+++ 03-Express.JS/3-Middleware/script.js
@@ -1,15 +1,7 @@
 import express from 'express';
 const app = express()
 
-// Middleware
-app.use(function(req , res, next){
-    console.log('Run Middleware')
-    next()
-})
-app.get('/' , (req, res) => {
-    res.send('Hello World from Middleware')
-})
+// Built in Middleware (Body Parser)
+app.use(express.json());
 
 app.listen(3000)
-
-// error handling in middleware
