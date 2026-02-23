8e2c563a576146d74df73dbe4fdf020a5f111579
CodeQueen-AI <codeq209@gmail.com>
2026-02-23 14:45:37 +0500

    update:

--- 03-Express.JS/2-Dynamic Routes/script.js
+++ 03-Express.JS/2-Dynamic Routes/script.js
@@ -5,5 +5,11 @@ app.get('/' , (req, res) => {
     res.send('Dynamic Routes')
 })
 
+// Dynamic Route
+app.get("/profile/:name", (req, res) => {
+    const userName = req.params.name;
+    res.send(`Welcome, ${userName}`);
+});
+
 app.listen(3000)
 
