cf5225f8f69183574372a13ce85e6881a714881c
CodeQueen-AI <codeq209@gmail.com>
2026-02-24 00:00:57 +0500

    update

--- 03-Express.JS/4-Cookies/script.js
+++ 03-Express.JS/4-Cookies/script.js
@@ -8,4 +8,9 @@ app.get('/', (req, res) => {
     res.send("Home Page");
 });
 
+app.get('/set', (req, res) => {
+    res.cookie("username", "CodeQueen");
+    res.send("Cookie Set");
+});
+
 app.listen(3000)
