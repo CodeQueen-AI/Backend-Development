2a97e112cb11310f28ae24d02eab39ce4a161ea3
CodeQueen-AI <codeq209@gmail.com>
2026-02-23 15:34:54 +0500

    update

--- 03-Express.JS/3-Middleware/script.js
+++ 03-Express.JS/3-Middleware/script.js
@@ -4,4 +4,10 @@ const app = express()
 // Built in Middleware (Body Parser)
 app.use(express.json());
 
+// Global Middleware
+app.use((req, res, next) => {
+    console.log("Global Middleware Running");
+    next(); 
+});
+
 app.listen(3000)
