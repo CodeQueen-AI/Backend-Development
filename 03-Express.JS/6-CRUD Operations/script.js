58d5650bc636a6b33cb10a84ed4ff0503b8fa203
CodeQueen-AI <codeq209@gmail.com>
2026-02-24 20:24:52 +0500

    update

---
+++ 03-Express.JS/6-CRUD Operations/script.js
@@ -0,0 +1,19 @@
+import express from 'express';
+const app = express()
+app.use(express.json());
+
+// Fake database (array)
+let users = [];
+
+
+// 1-Crate -> Add new user
+app.post('/users', (req, res) => {
+    const user = req.body;
+    users.push(user);
+    res.json({
+        message: "User Created Successfully",
+        users
+    });
+});
+
+app.listen(3000)
