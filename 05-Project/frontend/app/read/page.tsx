f446b62e28a2e7d70dca01ed83b0a62ec67392fb
CodeQueen-AI <codeq209@gmail.com>
2026-02-26 13:32:45 +0500

    update

--- 05-Project/frontend/app/read/page.tsx
+++ 05-Project/frontend/app/read/page.tsx
@@ -1,6 +1,8 @@
-export default function ReadUsersPage() {
+"use client";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
 
-  // temporary dummy users
+export default function ReadUsersPage() {
   const users = [
     {
       id: 1,
@@ -30,6 +32,14 @@ export default function ReadUsersPage() {
 
   return (
     <div className="min-h-screen p-10">
+      <div className="mb-6">
+        <Link
+          href="/create-user" className="inline-flex items-center gap-2 text-[#FF3E9B] font-medium hover:underline
+            transition cursor-pointer">
+          <FaArrowLeft />
+          Back To Home
+        </Link>
+      </div>
 
       {/* Heading */}
       <h1 className="text-5xl font-semibold text-center mb-12">
@@ -38,19 +48,16 @@ export default function ReadUsersPage() {
 
       {/* Users Grid */}
       <div className="grid grid-cols-4 gap-8 max-w-6xl mx-auto">
-
         {users.map((user) => (
           <div
             key={user.id}
-            className="border p-5 text-center shadow-sm hover:shadow-md transition"
-          >
+            className="border p-5 text-center shadow-sm hover:shadow-md transition">
 
             {/* Image */}
             <img
               src={user.image}
               alt={user.name}
-              className="w-24 h-24 mx-auto rounded-full object-cover mb-4"
-            />
+              className="w-24 h-24 mx-auto rounded-full object-cover mb-4"/>
 
             {/* Name */}
             <h2 className="text-lg font-semibold">
@@ -66,44 +73,21 @@ export default function ReadUsersPage() {
             <div className="flex justify-center gap-3">
 
               {/* Edit */}
-              <button
-                className="
-                  px-4 py-1
-                  border-2
-                  border-[#FF3E9B]
-                  text-[#FF3E9B]
-                  cursor-pointer
-                  hover:bg-[#FF3E9B]
-                  hover:text-white
-                  transition
-                "
-              >
+              <button className="px-4 py-1 border-2 border-[#FF3E9B] bg-[#FF3E9B] text-white cursor-pointer
+                  hover:bg-white hover:text-[#FF3E9B] transition">
                 Edit
               </button>
 
               {/* Delete */}
-              <button
-                className="
-                  px-4 py-1
-                  border-2
-                  border-red-500
-                  text-red-500
-                  cursor-pointer
-                  hover:bg-red-500
-                  hover:text-white
-                  transition
-                "
-              >
+              <button className="px-4 py-1 border-2 border-red-500 bg-red-500 text-white cursor-pointer
+               hover:bg-white hover:text-red-500
+                  transition">
                 Delete
               </button>
-
             </div>
-
           </div>
         ))}
-
       </div>
-
     </div>
   );
 }
