d6a42d482746ee44f34335c103e57841bcf39d41
CodeQueen-AI <codeq209@gmail.com>
2026-02-26 12:46:44 +0500

    update

--- 05-Project/frontend/app/page.tsx
+++ 05-Project/frontend/app/page.tsx
@@ -1,37 +1,29 @@
 export default function CreateUserPage() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      
       <div className="p-8 w-full max-w-md">
-        
-        {/* Heading */}
         <h1 className="text-5xl font-bold text-center mb-10">
           Create Users
         </h1>
-
-        {/* Form */}
         <form className="flex flex-col gap-8">
-          
-          {/* Name */}
+            {/* Name */}
           <div>
             <label className="block text-sm font-medium mb-1">
               NAME
             </label>
             <input
               type="text"
-              className="w-full border-b border-gray-400 focus:outline-none focus:border-pink-500 py-1"
-            />
+              className="w-full border-b border-gray-400 focus:outline-none focus:border-pink-500 py-1"/>
           </div>
 
-          {/* Email */}
+            {/* Email */}
           <div>
             <label className="block text-sm font-medium mb-1">
               EMAIL
             </label>
             <input
               type="email"
-              className="w-full border-b border-gray-400 focus:outline-none focus:border-pink-500 py-1"
-            />
+              className="w-full border-b border-gray-400 focus:outline-none focus:border-pink-500 py-1"/>
           </div>
 
           {/* Image URL */}
@@ -41,40 +33,15 @@ export default function CreateUserPage() {
             </label>
             <input
               type="text"
-              className="w-full border-b border-gray-400 focus:outline-none focus:border-pink-500 py-1"
-            />
+              className="w-full border-b border-gray-400 focus:outline-none focus:border-pink-500 py-1"/>
           </div>
 
-          {/* Button */}
-          {/* <button
-            type="submit"
-            className="mt-6 text-white font-semibold py-2 rounded-lg transition hover:opacity-90"
-            style={{ backgroundColor: "#FF3E9B" }}>
-            Create User
-          </button> */}
-          <button
-  type="submit"
-  className="
-    mt-6
-    w-40
-    py-3
-    mx-auto
-    block
-    font-semibold
-    border-2
-    cursor-pointer
-    transition-all
-    duration-300
-    bg-white
-    text-[#FF3E9B]
-    border-[#FF3E9B]
-    hover:bg-[#FF3E9B]
-    hover:text-white
-    hover:border-white
-  "
->
-  Create User
-</button>
+            {/* Button */}
+          <button type="submit" className="mt-6 w-40 py-3 mx-auto block font-semibold border-2 cursor-pointer 
+          transition-all duration-300 bg-white text-[#FF3E9B] border-[#FF3E9B]  hover:bg-[#FF3E9B]
+           hover:text-white hover:border-white">
+                Create User
+            </button>
         </form>
       </div>
     </div>
