34ca3dd4a48d2767b5c7e652244fcbf5326f7bb8
CodeQueen-AI <codeq209@gmail.com>
2026-02-26 23:31:36 +0500

    update

--- 05-Project/backend/script.js
+++ 05-Project/backend/script.js
@@ -1,106 +1,79 @@
-// import express from 'express';
-// import userModel from './models/user.js'; 
-
-// const app = express();
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// // Read
-// app.get('/read' , async (req , res) => {
-//     let users = await userModelfind();
-//     res.render('read' , {users})
-// })
-
-// // Create
-// app.post('/create' , async (req , res) => {
-//     let {name, email, image} = req.body;
-
-//     let createdUser = await userModel.create({
-//         name,
-//         email, 
-//         image
-//     })
-
-//     res.send((createdUser))
-// })
-
-// // Edit
-// app.get('edit/:userid' , async (req, res) => {
-//     userModel.findOne({_id: req.params.user})
-//     res.render('edit' , {user})
-// })
-
-// // Delete
-// app.get('delete/:id' , async (req, res) => {
-//     let users = await userModel.findOneAndDelete({_id: req.params.user})
-//     res.redirect('read')
-// })
-
-// app.listen(5000)
-
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import userModel from "./models/user.js";
-
 const app = express();
 
-// middleware
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// allow frontend
+// Allow frontend
 app.use(cors({
   origin: "http://localhost:3000"
 }));
 
-// connect mongodb (ONLY ONCE)
-mongoose.connect("mongodb://127.0.0.1:27017/userDB")
-.then(() => {
-  console.log("MongoDB connected");
-})
-.catch((err) => {
-  console.log("MongoDB connection error:", err);
-});
+// // connect mongodb (ONLY ONCE)
+// mongoose.connect("mongodb://127.0.0.1:27017/userDB")
+// .then(() => {
+//   console.log("MongoDB connected");
+// })
+// .catch((err) => {
+//   console.log("MongoDB connection error:", err);
+// });
 
 
-// TEST ROUTE (IMPORTANT for debugging)
-app.get("/", (req, res) => {
-  res.json({
-    success: true,
-    message: "Backend working"
-  });
-});
+// // TEST ROUTE (IMPORTANT for debugging)
+// app.get("/", (req, res) => {
+//   res.json({
+//     success: true,
+//     message: "Backend working"
+//   });
+// });
 
 
-// CREATE USER
+// Create User
+// app.post("/create", async (req, res) => {
+//   try {
+//     const { name, email, image } = req.body;
+//     const createdUser = await userModel.create({
+//       name,
+//       email,
+//       image
+//     });
+
+//     res.status(200).json({
+//       success: true,
+//       message: "User created successfully",
+//       user: createdUser
+//     });
+
+//   } catch (err) {
+
+//     console.log(err);
+
+//     res.status(500).json({
+//       success: false,
+//       message: err.message
+//     });
+
+//   }
+
+// });
+
+// Create Users
 app.post("/create", async (req, res) => {
-  try {
-    const { name, email, image } = req.body;
-    const createdUser = await userModel.create({
-      name,
-      email,
-      image
-    });
-
-    res.status(200).json({
-      success: true,
-      message: "User created successfully",
-      user: createdUser
-    });
-
-  } catch (err) {
-
-    console.log(err);
-
-    res.status(500).json({
-      success: false,
-      message: err.message
-    });
-
-  }
-
+  const { name, email, image } = req.body;
+  const createdUser = await userModel.create({
+    name,
+    email,
+    image
+  });
+  res.status(200).json({
+    success: true,
+    message: "User created successfully",
+    user: createdUser
+  });
 });
 
 
