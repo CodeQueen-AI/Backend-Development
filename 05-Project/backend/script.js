42be5f78973d234b30f6557f5087ca241134136c
CodeQueen-AI <codeq209@gmail.com>
2026-02-26 15:39:16 +0500

    update

--- 05-Project/backend/script.js
+++ 05-Project/backend/script.js
@@ -6,11 +6,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Read
 app.get('/read' , async (req , res) => {
     let users = await userModelfind();
     res.render('read' , {users})
 })
 
+// Create
 app.post('/create' , async (req , res) => {
     let {name, email, image} = req.body;
 
@@ -23,8 +25,15 @@ app.post('/create' , async (req , res) => {
     res.send((createdUser))
 })
 
-app.getMaxListeners('edit/:userid' , async (req, res) => {
+// Edit
+app.get('edit/:userid' , async (req, res) => {
     userModel.findOne({_id: req.params.user})
+})
+
+// Delete
+app.get('delete/:id' , async (req, res) => {
+    let users = await userModel.findOneAndDelete({_id: req.params.user})
+    res.redirect('read')
 })
 
 app.listen(5000)
