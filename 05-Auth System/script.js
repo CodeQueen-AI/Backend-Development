8ae95d103f0ff174dc88909d2534e12442a52b09
CodeQueen-AI <codeq209@gmail.com>
2026-02-27 21:46:17 +0500

    update

--- 05-Auth System/script.js
+++ 05-Auth System/script.js
@@ -3,8 +3,9 @@ const app = express()
 
 // Home Page
 app.get('/' , (req, res) => {
-    res.cookies("name" , "harsh");
+    res.cookie("name" , "harsh");
     res.send("Done")
 })
 
 app.listen(3000)
+
