1ea330be95f553e2cf436dcb1bea6fa359571d93
CodeQueen-AI <codeq209@gmail.com>
2026-02-27 21:50:38 +0500

    update

--- 05-Auth System/script.js
+++ 05-Auth System/script.js
@@ -1,10 +1,17 @@
 import express from 'express';
 const app = express()
+const cookieParser = require('cookie-parser')
 
-// Home Page
+app.use(cookieParser())
+
 app.get('/' , (req, res) => {
     res.cookie("name" , "harsh");
     res.send("Done")
+})
+
+app.get('/' , (req, res) => {
+    console.log(req.cookies)
+    res.send("Read Page")
 })
 
 app.listen(3000)
