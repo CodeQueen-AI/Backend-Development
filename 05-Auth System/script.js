e22ee0bba1af938cf29f4bc7c947d8dc1d2df60a
CodeQueen-AI <codeq209@gmail.com>
2026-02-27 22:19:36 +0500

    update

--- 05-Auth System/script.js
+++ 05-Auth System/script.js
@@ -1,13 +1,17 @@
 import express from 'express';
-import bcrypt from 'bcrypt';
+import cookieParser from 'cookie-parser';
 const jwt = require('sonwebtoken')
 const app = express();
 
+app.use(cookieParser())
 app.get('/', (req, res) => {
     let token = jwt.sign({email: "harsh@example.com"}, "secret");
     res.cookie("token" , token)
     res.send("Done")
 });
 
+app.get("/read", function (req, res) {
+    console.log(res.cookies.token)
+})
 
 app.listen(3000)
