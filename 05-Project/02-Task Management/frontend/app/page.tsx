52948cdfe1e19cca379dd34cbdc0e8c9d67ad9d9
CodeQueen-AI <codeq209@gmail.com>
2026-02-27 15:04:32 +0500

    update

--- 05-Project/02-Task Management/frontend/app/page.tsx
+++ 05-Project/02-Task Management/frontend/app/page.tsx
@@ -1,65 +1,43 @@
-import Image from "next/image";
+"use client";
+import Link from "next/link";
+import { FaArrowRight } from "react-icons/fa";
 
-export default function Home() {
+export default function UserPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <div className="min-h-screen">
+      <div className="p-6">
+        <Link
+          href="/read"
+          className="inline-flex items-center gap-2 text-[#FF3E9B] font-medium hover:underline transition 
+          cursor-pointer">
+          All Tasks
+          <FaArrowRight />
+        </Link>
+      </div>
+
+      <div className="flex items-center justify-center">
+        <div className="p-8 w-full max-w-md">
+          <h1 className="text-5xl font-semibold text-center mb-6 text-[#FB2576]">
+            Add To Task
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+          <form className="flex flex-col gap-8">
+            <div>
+              <label className="block text-sm font-medium mb-1">TASK</label>
+              <input
+                type="text"
+                className="w-full border-b border-gray-400 focus:outline-none focus:border-pink-500 py-1"
+                required/>
+            </div>
+
+            <button
+              type="submit"
+              className="mt-6 w-40 py-3 mx-auto block font-semibold border-2 cursor-pointer transition-all 
+                duration-300">
+              Add To Task
+            </button>
+          </form>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
-  );
+  )
 }
