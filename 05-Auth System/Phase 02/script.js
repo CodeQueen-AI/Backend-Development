623d97d2c6de0b85f0f981294cce1cb655637929
CodeQueen-AI <codeq209@gmail.com>
2026-03-01 19:56:42 +0500

    update

--- 05-Auth System/Phase 02/script.js
+++ 05-Auth System/Phase 02/script.js
@@ -1,7 +1,9 @@
 import express from 'express';
+const cookieParser = require('cookie-parser')
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended : true}));
+app.use(cookieParser())
 
 app.get('/' , (req, res) => {
     res.send("Welcome")
