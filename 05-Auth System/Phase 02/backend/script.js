ba6eefeebce12eaabf4e30ec47553856b04b3abe
CodeQueen-AI <codeq209@gmail.com>
2026-03-01 23:31:25 +0500

    update

--- 05-Auth System/Phase 02/backend/script.js
+++ 05-Auth System/Phase 02/backend/script.js
@@ -1,27 +1,49 @@
-import express from 'express';
-import userModel from './userModels.js'; 
-const cookieParser = require('cookie-parser')
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended : true}));
-app.use(cookieParser())
+import express from "express";
+import userModel from "./userModels.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
 
-app.get('/' , (req, res) => {
-    res.send("Welcome")
-})
+const app = express();
 
-app.post('/create' , (req, res) => {
-    let {username, email, password, age} = req.body;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-    bycrypt.getSalt(10, (err, salt) => {
-        bycrypt.hash(password , salt, async(err, hash) =>
-            let createdUser = await userModel.create({
-                username,
-                email,
-                password:hash,
-    })
-    JsonWebTokenError.sign({email}, "shhhhh")
-    res.send((createdUser))
-})
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+// Create
+app.post("/create", async (req, res) => {
+  try {
+    let { username, email, password, age } = req.body;
+
+    // Generate salt
+    const salt = await bcrypt.genSalt(10);
+
+    // Hash password
+    const hash = await bcrypt.hash(password, salt);
+
+    // 3️⃣ Create user
+    const createdUser = await userModel.create({
+      username,
+      email,
+      password: hash,
+      age
+    });
+
+    // Create token
+    const token = jwt.sign({ email }, "shhhhhsecretkey");
+
+    // Send token in cookie
+    res.cookie("token", token);
+
+    res.send(createdUser);
+
+  } catch (error) {
+    res.status(500).send("Error creating user");
+  }
+});
 
 app.listen(3000)
