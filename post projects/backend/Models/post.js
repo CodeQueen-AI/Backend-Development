489dd500286aa325529f6a44f8b5e23ea60fb0a9
CodeQueen-AI <codeq209@gmail.com>
2026-03-02 23:17:14 +0500

    update

--- post projects/backend/Models/post.js
+++ post projects/backend/Models/post.js
@@ -9,7 +9,14 @@ const postSchema = mongoose.Schema({
     date: {
         type: Date,
         default: Date.now
-    }
+    },
+    content: String,
+    likes: [
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    ]
 });
 
 const userModel = mongoose.model('post', postSchema);
