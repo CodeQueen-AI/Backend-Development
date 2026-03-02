99e0b700656fc220c4e09860c86e251860912013
CodeQueen-AI <codeq209@gmail.com>
2026-03-02 22:27:44 +0500

    update

--- 04-Mongo DB/3-Data Association/script.js
+++ 04-Mongo DB/3-Data Association/script.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import userModel from './Models/user.js'; 
+import postModel from './Models/post.js'; 
 const app = express()
 
 app.get('/' , (req, res) => {
     res.send("Welcome")
+})
+
+app.get('/create' , (req, res) => {
+    userModel.create({
+        username: "codequeen", 
+        age: 18,
+        email: "codequeen",
+    })
 })
 
 app.listen(3000)
