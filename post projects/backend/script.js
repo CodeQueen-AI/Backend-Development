7e25c620a961e20b2be08fc4b63b84a086d8b3aa
CodeQueen-AI <codeq209@gmail.com>
2026-03-03 15:31:38 +0500

    update

--- post projects/backend/script.js
+++ post projects/backend/script.js
@@ -34,6 +34,15 @@ app.get('/like/:id', isLoggedIn , (req, res) => {
     res.redirect("/profile")
 })
 
+app.get('/edit/:id', isLoggedIn , (req, res) => {
+    let post = await userModel.findOne({_id: req.params.id}).populate("user");
+    res.render("edit" , {post})
+})
+
+app.get('/update/:id', isLoggedIn , (req, res) => {
+    let post = await userModel.findOneAndUpdate({_id: req.params.id}, {content: req.body.content});
+    res.redirect("/profile"})
+})
 
 app.post('/post', isLoggedIn , async(req, res) => {
     let user = await userModel.findOne({email: req.user.email}).populate("posts")
