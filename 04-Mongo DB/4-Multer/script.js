df6a7c69cf9898787afe4b4636b4105c7b9d9d39
CodeQueen-AI <codeq209@gmail.com>
2026-03-03 16:02:36 +0500

    update

---
+++ 04-Mongo DB/4-Multer/script.js
@@ -0,0 +1,14 @@
+import express from "express";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+app.use("/api", userRoutes);
+
+app.listen(3000)
