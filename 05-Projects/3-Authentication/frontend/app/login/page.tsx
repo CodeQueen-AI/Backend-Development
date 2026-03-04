9a4fb8cef5df7ffc44dfa6086445c325574629bb
CodeQueen-AI <codeq209@gmail.com>
2026-03-04 16:24:18 +0500

    update

--- 05-Projects/3-Authentication/frontend/app/login/page.tsx
+++ 05-Projects/3-Authentication/frontend/app/login/page.tsx
@@ -1,23 +1,15 @@
-import { Poppins } from "next/font/google";
 import Link from "next/link";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-});
 
 export default function LoginPage() {
   return (
-    <div className={`${poppins.className} min-h-screen flex items-center justify-center`}>
+    <div className="min-h-screen flex items-center justify-center">
       <div className="p-5 w-full max-w-md">
-
         {/* Heading */}
         <h1 className="text-4xl font-semibold text-center text-[#261CC1] mb-8">
           Welcome Back
         </h1>
 
         <form className="space-y-4">
-
           {/* Email */}
           <div>
             <label className="block text-sm font-medium mb-1">
@@ -43,14 +35,9 @@ export default function LoginPage() {
           {/* Button */}
           <button
             type="submit"
-            className="w-1/2 mx-auto block 
-                       bg-white text-[#261CC1] 
-                       border-2 border-[#261CC1] 
-                       py-3 font-medium 
-                       hover:bg-[#261CC1] hover:text-white hover:border-white 
-                       transition-all duration-300 
-                       mt-6 cursor-pointer"
-          >
+            className="w-1/2 mx-auto block bg-white text-[#261CC1] border-2 border-[#261CC1] py-3 
+            font-medium hover:bg-[#261CC1] hover:text-white hover:border-white transition-all duration-300 
+            mt-6 cursor-pointer">
             Login
           </button>
 
@@ -60,15 +47,12 @@ export default function LoginPage() {
               Don’t have an account?{" "}
               <Link
                 href="/"
-                className="text-[#261CC1] font-medium hover:underline"
-              >
+                className="text-[#261CC1] font-medium hover:underline">
                 Create Account
               </Link>
             </p>
           </div>
-
         </form>
-
       </div>
     </div>
   );
